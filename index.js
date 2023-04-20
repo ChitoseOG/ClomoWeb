caf9f8893636103cb56a4f84116562a34b3c47c2
chitoseog <130519880+chitoseog@users.noreply.github.com>
2023-04-20 11:32:47 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var timerId;
+ 
+// ボックスを表示して、タイマーを開始
+   document.getElementById("temporaryBox").style.display = "block"; // ボックスを表示
+   timerId = setTimeout( closeBox , 5000 ); // タイマーを開始
+
+ 
+// ボックスを消して、タイマーを終了
+   document.getElementById("temporaryBox").style.display = "none"; // ボックスを消す
+   clearTimeout( timerId ); // タイマーを終了
